747f502f647d8025ef8d2471e3461ec33e1455b2
xbony2 <xbony22@gmail.com>
2015-07-27 20:31:12 -0400

    copying VikeStep (sort of), allow it's not even loading which really pisses me the fuck off

---
+++ src/main/java/xbony2/aepm/AEPMClassTransformer.java
@@ -0,0 +1,70 @@
+package xbony2.aepm;
+
+import org.objectweb.asm.Opcodes;
+
+import org.objectweb.asm.tree.VarInsnNode;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+import cpw.mods.fml.common.FMLLog;
+
+import java.util.Arrays;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+
+public class AEPMClassTransformer implements IClassTransformer {
+	public static final String TRANSFORMED_CLASS = "net.minecraft.entity.boss.EntityDragon";
+	public static final String TRANSFORMED_METHOD_DEOBFUSCATED  = "createEnderPortal";
+	public static final String TRANSFORMED_METHOD_OBFUSCATED = "b";
+	public static final String TRANSFORMED_DESC_OBFUSCATED = "";
+	public static final String TRANSFORMED_DESC_DEOBFUSCATED = "";
+	
+	@Override
+	public byte[] transform(String name, String transformedName, byte[] basicClass){
+		boolean isObfuscated = !name.equals(transformedName);
+		return TRANSFORMED_CLASS != transformedName ? transform(basicClass, isObfuscated) : basicClass;
+	}
+	
+	public byte[] transform(byte[] classBeingTransformed, boolean isObfuscated){
+		FMLLog.info("Transforming!");
+		try {
+			ClassNode node = new ClassNode();
+			ClassReader reader = new ClassReader(classBeingTransformed);
+			reader.accept(node, 0);
+			
+			//Trasformeration here
+			final String CREATE_ENDER_PORTAL = isObfuscated ? TRANSFORMED_METHOD_OBFUSCATED : TRANSFORMED_METHOD_DEOBFUSCATED;
+			final String CREATE_ENDER_PORTAL_DESC = "(II)V"; //Pretty sure it's the same, I dunno these things.
+			
+			for(MethodNode method : node.methods){
+				if(method.name.equals(CREATE_ENDER_PORTAL) && method.desc.equals(CREATE_ENDER_PORTAL_DESC)){
+					AbstractInsnNode targetNode = null;
+					for(AbstractInsnNode instruction : method.instructions.toArray()){
+						/*if(instruction.getOpcode() == Opcodes.ALOAD){
+							if(((VarInsnNode) instruction).var == 5 && instruction.getNext().getOpcode() == Opcodes.GETSTATIC){
+								targetNode = instruction;
+								break;
+							}
+						}*/
+						method.instructions.remove(instruction);// Removes ALL OF THE INSTRUCTIONS
+					}
+					
+					/*if(targetNode != null){
+						
+					}else{
+						FMLLog.info("Something broke, but who cares? Everyone? Sorry then.");
+					}*/
+				}
+			}
+			
+			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+			node.accept(writer);
+			return writer.toByteArray();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return classBeingTransformed;
+	}
+}
---
+++ src/main/java/xbony2/aepm/AEPMPlugin.java
@@ -0,0 +1,36 @@
+package xbony2.aepm;
+
+import cpw.mods.fml.relauncher.IFMLCallHook;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
+
+import java.util.Map;
+
+@MCVersion("1.7.10")
+@TransformerExclusions("xbony2.aepm")
+public class AEPMPlugin implements IFMLLoadingPlugin {
+
+	@Override
+	public String[] getASMTransformerClass(){
+		return new String[]{"xbony2.aepm.AEPMClassTransformer"};
+	}
+
+	@Override
+	public String getModContainerClass(){
+		return "xbony2.aepm.AEPMContainer";
+	}
+
+	@Override
+	public String getSetupClass(){
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data){}
+
+	@Override
+	public String getAccessTransformerClass(){
+		return null;
+	}
+}
